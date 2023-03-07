ffce8df664ad73adfb26a07bc75cf0f2885567af
Thugi Nirbi Alam <thuginirbialam@gmail.com>
2023-03-07 12:35:08 +0700

    feat(grid system): add page for test grid

---
+++ resources/views/test-grid.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="text-center p-4">Test Grid</h1>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-3 p-2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti quo velit reiciendis officia, ipsum veritatis fugiat, veniam hic ipsa unde earum consequatur, voluptas commodi eaque. Saepe accusamus id qui nobis.
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 p-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore adipisci illum quibusdam repellat eveniet veniam officiis error quaerat porro cum, soluta neque blanditiis dolor rem perferendis harum similique ea optio.
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 p-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis animi delectus ipsam deserunt? Earum suscipit enim porro, accusantium cumque dolorem a error nisi, temporibus aliquid, hic voluptatibus vel animi ducimus.
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 p-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quibusdam at earum expedita vitae aliquid quisquam, cumque facere quasi reprehenderit quidem culpa error amet mollitia eius commodi enim voluptate quo?
+        </div>
+    </div>
+</div>
+@endsection
